b56bbaf00d7344601e6468812329ae6ef96b2010
Senguttuvan Mahalingam <rmsmech@gmail.com>
2024-04-21 23:52:47 +0400

    work

--- HaleyVue/volar.d.ts
+++ HaleyVue/volar.d.ts
@@ -1,5 +1,11 @@
+/// <reference types="vite/client" />
+
 import { MessageDisplayer } from "@haleyjs/vue";
-declare module '@haleyjs/vue"' {
+declare module '@haleyjs/vue' {
+    // import type { DefineComponent } from 'vue'
+    // const component: DefineComponent<{}, {}, any>
+    // export default component
+
     export interface GlobalComponents {
         MessageDisplayer  : typeof MessageDisplayer;
     }
--- HaleyVueTest/src/main.ts
+++ HaleyVueTest/src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import MessageDisplayer from '@haleyjs/vue';
+import Haley from "@haleyjs/vue";
 
 createApp(App)
-.use(MessageDisplayer)
+.use(Haley)
 .mount('#app');
