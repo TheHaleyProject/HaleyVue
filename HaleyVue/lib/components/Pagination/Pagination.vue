380019ab4154ad93c374eeb6b876f4b0f78dd3d1
Lingam <rmsmech@gmail.com>
2024-05-21 15:58:47 +0400

    pagination in progress

--- HaleyVue/lib/components/Pagination/Pagination.vue
+++ HaleyVue/lib/components/Pagination/Pagination.vue
@@ -1,198 +1,217 @@
 <template>
-    <div
-      class="flex-row items-center justify-between"
-      :class="totalItems > 0 ? 'flex' : 'hidden'"
-      v-if="(totalPages > 1 && hideItemsSize) || !hideItemsSize"
-    >
-      <nav class="order-1 flex items-center gap-3">
-        <i
-          class="bi bi-skip-start-fill cursor-pointer text-3xl text-sky-500 hover:text-orange-600"
-          @click="goToEnd(false)"
-          title="Go to First"
-        ></i>
-        <span
-          class="select-none hover:text-orange-600"
-          :class="
-            currentPage <= 1
-              ? 'pointer-events-none cursor-not-allowed text-slate-400'
-              : 'cursor-pointer text-sky-500'
-          "
-          @click="changePage(false)"
-          >Previous</span
+  <div
+    class="flex-row items-center justify-between"
+    :class="totalItems > 0 ? 'flex' : 'hidden'"
+    v-if="(totalPages > 1 && hideItemsSize) || !hideItemsSize"
+  >
+    <nav class="order-1 flex items-center gap-3">
+      <i
+        class="bi bi-skip-start-fill cursor-pointer text-3xl text-sky-500 hover:text-orange-600"
+        :class="
+          currentPage <= 1
+            ? 'pointer-events-none cursor-not-allowed text-slate-400'
+            : 'cursor-pointer text-sky-500'
+        "
+        @click="goToEnd(false)"
+        title="Go to First"
+      ></i>
+      <span
+        class="select-none hover:text-orange-600"
+        :class="
+          currentPage <= 1
+            ? 'pointer-events-none cursor-not-allowed text-slate-400'
+            : 'cursor-pointer text-sky-500'
+        "
+        @click="changePage(false)"
+        >Previous</span
+      >
+
+      <span
+        class="select-none hover:text-orange-600"
+        :class="
+          currentPage >= totalPages && !unCertain
+            ? 'pointer-events-none cursor-not-allowed text-slate-400'
+            : 'cursor-pointer text-sky-500'
+        "
+        @click="changePage(true)"
+        >Next</span
+      >
+
+      <i
+        class="bi bi-skip-end-fill cursor-pointer text-3xl text-sky-500 hover:text-orange-600"
+        :class="
+          currentPage >= totalPages
+            ? 'pointer-events-none cursor-not-allowed text-slate-400'
+            : 'cursor-pointer text-sky-500'
+        "
+        @click="goToEnd(true)"
+        title="Go to End"
+      ></i>
+      <span class="text-sm text-slate-500">Go to :</span>
+      <input
+        v-model="gotoPage"
+        type="text"
+        class="page-input no-outline w-14 bg-white text-slate-500 focus:border-gray-400"
+        @keypress="helpers.NumbersOnly"
+        @keypress.enter.exact="handleKeyEvent"
+      />
+      <select
+        v-if="!hideItemsSize"
+        @change="displaySizeChanged"
+        class="h-8 w-14 rounded-md border-[1px] border-gray-200 bg-white px-1 outline-none"
+        v-model="displaySize"
+      >
+        <option v-if="userPageSizeValid">
+          {{ itemsPerPage }}
+        </option>
+        <option v-for="(ps, index) in pageSizeOptions" :key="index" :value="ps">
+          {{ ps }}
+        </option>
+      </select>
+    </nav>
+
+    <div class="flex items-center gap-4 text-left text-sm text-slate-500">
+      <div>
+        Page
+        <span class="font-medium text-slate-600"> {{ currentPage }} </span>
+        <span v-show="!unCertain">
+          of
+          <span class="font-medium text-slate-600">{{ totalPages }}</span></span
         >
-  
-        <span
-          class="select-none hover:text-orange-600"
-          :class="
-            currentPage >= totalPages
-              ? 'pointer-events-none cursor-not-allowed text-slate-400'
-              : 'cursor-pointer text-sky-500'
-          "
-          @click="changePage(true)"
-          >Next</span
-        >
-  
-        <i
-          class="bi bi-skip-end-fill cursor-pointer text-3xl text-sky-500 hover:text-orange-600"
-          @click="goToEnd(true)"
-          title="Go to End"
-        ></i>
-        <span class="text-sm text-slate-500">Jump to :</span>
-        <input
-          v-model="gotoPage"
-          type="text"
-          class="page-input no-outline w-14 bg-white text-slate-500 focus:border-gray-400"
-          v-on:keyup="handleKeyEvent"
-        />
-        <select
-          v-if="!hideItemsSize"
-          @change="displaySizeChanged"
-          class="h-8 w-14 rounded-md border-[1px] border-gray-200 bg-white px-1 outline-none"
-          v-model="displaySize"
-        >
-          <option v-if="userPageSizeValid">
-            {{ itemsPerPage }}
-          </option>
-          <option v-for="(ps,index) in pageSizeOptions" :key="index" :value="ps"> {{ ps }}</option>
-        </select>
-      </nav>
-  
-      <div class="flex items-center gap-4 text-left text-sm text-slate-500">
-        <div>
-          Page
-          <span class="font-medium text-slate-600"> {{ currentPage }} </span> of
-          <span class="font-medium text-slate-600">{{ totalPages }}</span>
-        </div>
-        <div>
-          Items :
-          <span class="font-medium text-slate-600">{{ totalItems }}</span>
-        </div>
+      </div>
+      <div v-show="!unCertain">
+        Total Items :
+        <span class="font-medium text-slate-600">{{ totalItems }}</span>
       </div>
     </div>
-  </template>
-  
-  <script setup lang="ts">
-  import {watch,onMounted,computed,ref}from 'vue';
-  import * as helpers from "@functions";
-  
-  interface Props {
-    totalItems: number;
-    itemsPerPage?: number;
-    activePage?: number;
-    hideItemsSize?: boolean;
-    autoHide?:boolean;
-  }
-  
-  const _props = withDefaults(defineProps<Props>(), {
-    itemsPerPage: 12,
-    totalItems: 1,
-    activePage: 1,
-    hideItemsSize: false,
-    autoHide:true
-  });
-  
-  const pageSizeOptions = [10, 20, 50, 75, 100];
-  const userPageSizeValid = computed<boolean>(() => {
-    return (
-      _props.itemsPerPage != null && //User value is not null.
-      helpers.isNumber(_props.itemsPerPage) && //User value is numeric
-      ![10, 20, 50, 75, 100].includes(_props.itemsPerPage) && //user value is not already present.
-      _props.itemsPerPage > 3 //Atleast 3 should be displayed per page.
-    );
-  });
-  const displaySize = ref<number>(userPageSizeValid ? _props.itemsPerPage :pageSizeOptions[0]);
-  const currentPage = ref<number>(1);
-  const totalPages = computed<number>(() =>
-    Math.ceil(
-      (_props.totalItems <= 0 ? 1 : _props.totalItems) /
-        (displaySize.value <= 0 ? 1 : displaySize.value)
-    )
+  </div>
+</template>
+
+<script setup lang="ts">
+import { watch, onMounted, computed, ref } from "vue";
+import * as helpers from "@functions";
+
+interface Props {
+  totalItems: number;
+  itemsPerPage?: number;
+  activePage?: number;
+  hideItemsSize?: boolean;
+  autoHide?: boolean;
+  unCertain?: boolean;
+}
+
+const _props = withDefaults(defineProps<Props>(), {
+  itemsPerPage: 12,
+  totalItems: 1,
+  activePage: 1,
+  hideItemsSize: false,
+  autoHide: true,
+  unCertain: false,
+});
+
+const pageSizeOptions = [10, 20, 50, 75, 100];
+const userPageSizeValid = computed<boolean>(() => {
+  return (
+    _props.itemsPerPage != null && //User value is not null.
+    helpers.isNumeric(_props.itemsPerPage) && //User value is numeric
+    ![10, 20, 50, 75, 100].includes(_props.itemsPerPage) && //user value is not already present.
+    _props.itemsPerPage > 3 //Atleast 3 should be displayed per page.
   );
-  const activePageChangeRequest = computed<number>(() => _props.activePage);
-  const gotoPage = ref<any>();
-  
-  const emit = defineEmits<{
-    (e: "changePage", page: number, itemsCount: number): void;
-  }>();
-  
-  function coerceCurrentPage() {
-    //Coerce the values
-    if (currentPage.value < 1) {
-      currentPage.value = 1;
-    } else if (currentPage.value > totalPages.value) {
-      currentPage.value = totalPages.value;
-    }
-  
-    //Try to emit the event.
-    emit("changePage", currentPage.value, displaySize.value);
+});
+const displaySize = ref<number>(
+  userPageSizeValid ? _props.itemsPerPage : pageSizeOptions[0],
+);
+const currentPage = ref<number>(1);
+const totalPages = computed<number>(() =>
+  Math.ceil(
+    (_props.totalItems <= 0 ? 1 : _props.totalItems) /
+      (displaySize.value <= 0 ? 1 : displaySize.value),
+  ),
+);
+const activePageChangeRequest = computed<number>(() => _props.activePage);
+const gotoPage = ref<any>();
+
+const emit = defineEmits<{
+  (e: "changePage", page: number, itemsCount: number): void;
+}>();
+
+function coerceCurrentPage() {
+  //Coerce the values
+  if (currentPage.value < 1) {
+    currentPage.value = 1;
+  } else if (currentPage.value > totalPages.value && !(_props.unCertain)) {
+    currentPage.value = totalPages.value;
   }
-  
-  function handleKeyEvent(e: any) {
-    if (!(e?.key === "Enter" || e?.keyCode == 13)) return;
-    //Change page and then request change
-    if (helpers.isNumber(gotoPage.value)) {
-      //If numeric, then parse and change value.
-      let requested = parseInt(gotoPage.value);
-      if (requested == currentPage.value) return; // Do not try to emit if we are processing same values.
-  
-      currentPage.value = requested;
-    }
-    //After you handle , clean the text.
-    gotoPage.value = "";
+
+  //Try to emit the event.
+  emit("changePage", currentPage.value, displaySize.value);
+}
+
+function handleKeyEvent(e: any) {
+  // if (!(e?.key === "Enter" || e?.keyCode == 13)) return; //we are direclty handling with vue provider.
+  //Change page and then request change
+  if (helpers.isNumeric(gotoPage.value)) {
+    //If numeric, then parse and change value.
+    let requested = parseInt(gotoPage.value);
+    if (requested == currentPage.value) return; // Do not try to emit if we are processing same values.
+
+    currentPage.value = requested;
   }
-  
-  function displaySizeChanged() {
-    coerceCurrentPage();
+  //After you handle , clean the text.
+  gotoPage.value = "";
+}
+
+function displaySizeChanged() {
+  coerceCurrentPage();
+}
+
+function changePage(go_forward: boolean) {
+  if (go_forward) {
+    currentPage.value++;
+  } else {
+    currentPage.value--;
   }
-  
-  function changePage(go_forward: boolean) {
-    if (go_forward) {
-      currentPage.value++;
-    } else {
-      currentPage.value--;
-    }
+}
+
+function goToEnd(go_right: boolean) {
+  if (go_right) {
+    currentPage.value = totalPages.value;
+  } else {
+    currentPage.value = 1;
   }
-  
-  function goToEnd(go_right: boolean) {
-    if (go_right) {
-      currentPage.value = totalPages.value;
-    } else {
-      currentPage.value = 1;
-    }
+}
+
+watch(currentPage, () => {
+  coerceCurrentPage();
+});
+
+watch(totalPages, () => {
+  //Totalpages only change when we initialize or the total items count changes.
+  coerceCurrentPage();
+});
+
+watch(activePageChangeRequest, () => {
+  if (helpers.isNumeric(_props.activePage)) {
+    //If numeric, then parse and change value.
+    let requested = Math.ceil(_props.activePage);
+    if (requested == currentPage.value) return; // Do not try to emit if we are processing same values.
+
+    currentPage.value = requested;
   }
-  
-  watch(currentPage, () => {
-    coerceCurrentPage();
-  });
-  
-  watch(totalPages, () => {
-    //Totalpages only change when we initialize or the total items count changes.
-    coerceCurrentPage();
-  });
-  
-  watch(activePageChangeRequest, () => {
-    if (helpers.isNumber(_props.activePage)) {
-      //If numeric, then parse and change value.
-      let requested = Math.ceil(_props.activePage);
-      if (requested == currentPage.value) return; // Do not try to emit if we are processing same values.
-  
-      currentPage.value = requested;
-    }
-  });
-  
-  onMounted(()=>{
-    coerceCurrentPage();
-  })
-  </script>
-  
-  <style scoped>
-  .page-input {
-    @apply inline-flex h-7 select-none items-center justify-center rounded border-0 px-1 text-sm outline-none  transition duration-150 ease-in-out;
-    box-shadow: 0px 0px 2px 0px rgb(165, 164, 164);
-  }
-  
-  select option {
-    background-color: white;
-  }
-  </style>
-  
+});
+
+onMounted(() => {
+  coerceCurrentPage();
+});
+</script>
+
+<style scoped>
+.page-input {
+  @apply inline-flex h-7 select-none items-center justify-center rounded border-0 px-1 text-sm outline-none  transition duration-150 ease-in-out;
+  box-shadow: 0px 0px 2px 0px rgb(165, 164, 164);
+}
+
+select option {
+  background-color: white;
+}
+</style>
--- HaleyVue/lib/functions/helperFunctions.ts
+++ HaleyVue/lib/functions/helperFunctions.ts
@@ -1,6 +1,17 @@
-export function isNumber(value?: string | number): boolean
-{
-   return ((value != null) &&
-           (value !== '') &&
-           !isNaN(Number(value.toString())));
+export function isNumeric(value: any): boolean {
+  return /^-?\d+$/.test(value); // Can be a positive or negative number
+}
+
+export function NumbersOnly(args: any): boolean {
+  if (args.charCode == null) return false;
+  if (
+    args.charCode > 31 &&
+    (args.charCode < 48 || args.charCode > 57) &&
+    args.charCode !== 46
+  ) {
+    args.preventDefault(); //prevent further action
+  } else {
+    return true;
+  }
+  return false;
 }
